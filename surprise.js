52cf4a63eeb0930b249b3bbdfeb279cf5d309cca
Itszkall <150927942+Itszkall@users.noreply.github.com>
2023-11-15 20:51:12 +0700

    Add files via upload

---
+++ surprise.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Tambahkan efek kejutan disini
+    setTimeout(function () {
+        alert("Yahahaha! kaget ga deek?");
+    }, 5000); // Prank muncul setelah 5 detik
+});
